b9b42d5c18790dac7a708f59b55155942330416d
github-xuanwang <35718382+github-xuanwang@users.noreply.github.com>
2022-10-12 01:33:33 -0400

    update the repository with the files from the final project

---
+++ src/main/java/com/jac/project/WebProject.java
@@ -0,0 +1,13 @@
+package com.jac.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProject.class, args);
+	}
+
+}
